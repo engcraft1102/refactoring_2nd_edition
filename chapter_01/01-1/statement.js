73cca13b6d485a670c1dfb397435424dd3b53759
최준원 <cjw1102@naver.com>
2022-04-25 00:52:46 +0900

    1.5 중간 점검

--- chapter_01/01-1/statement.js
+++ chapter_01/01-1/statement.js
@@ -1,7 +1,33 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  for (let perf of invoice.performances) {
+    // 청구 내역 출력
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
@@ -9,6 +35,16 @@ export function statement(invoice, plays) {
       currency: "USD",
       minumumFractionDigits: 2,
     }).format(aNumber / 100);
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+
+    return result;
   }
 
   function playFor(aPerformance) {
@@ -37,27 +73,4 @@ export function statement(invoice, plays) {
     }
     return result;
   }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
-    // 청구 내역 출력
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += amountFor(perf);
-  }
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
 }
