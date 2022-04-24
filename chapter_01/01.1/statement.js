e864d8cab143c4349ea49238561fa2ec35c46aa8
최준원 <cjw1102@naver.com>
2022-04-24 12:32:36 +0900

    01.1

---
+++ chapter_01/01.1/statement.js
@@ -0,0 +1,42 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const fotmat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minumumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performnaces) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르입니다: ${play.type}`);
+    }
+    // 포인트 적립
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역 출력
+    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
+    return result;
+  }
+}
