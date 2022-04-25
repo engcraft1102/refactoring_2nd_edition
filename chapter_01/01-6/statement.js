d4f45f73dfbf30d0e895cd1bb8c63b537d77a8e8
최준원 <cjw1102@naver.com>
2022-04-25 16:17:58 +0900

    invoice 매개변수 추출

--- chapter_01/01-6/statement.js
+++ chapter_01/01-6/statement.js
@@ -1,6 +1,13 @@
 export function statement(invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  for (let perf of invoice.performances) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
@@ -11,7 +18,7 @@ export function statement(invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
@@ -22,7 +29,7 @@ export function statement(invoice, plays) {
   function totalVolumeCredits() {
     let result = 0;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
