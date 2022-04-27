f010d59e63ef83307d61764ea99504c3da7d96cd
최준원 <cjw1102@naver.com>
2022-04-27 17:49:15 +0900

    volumeCredits 함수 옮기기

--- chapter_01/01-6/createStatementData.js
+++ chapter_01/01-6/createStatementData.js
@@ -26,6 +26,16 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -44,7 +54,7 @@ export default function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance); // shallow copy
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
